2c04fbfc0f65f9015fa9aa67e45b200c84d1b08e
leopauls <58358116+leopauls@users.noreply.github.com>
2023-08-11 12:07:01 +0100

    Market Vol Surf

--- main.py
+++ main.py
@@ -45,8 +45,7 @@ def read_excel(file_path='bbg.xlsx'):
 
 def plot_delta_vol(df, start=0, stop=30*365, save=False):
     """
-    Plots the delta volatility surface
-
+    Plots the delta volatility surface interpolated linearly
 
     format of df:
 
@@ -66,34 +65,25 @@ def plot_delta_vol(df, start=0, stop=30*365, save=False):
     10DPut  => .10
 
     """
-
     x_axis = [0.1, 0.25, 0.5, 0.75, 0.9]
     x_legend = ["10DP", "25DP", "ATM", "25DC", "10DC"]
     y_ticks = [365, 5*365, 10*365, 15*365, 25*365]
     y_legend = ["1Y", "5Y", "10Y", "15Y", "25Y"]
-
     df = df.loc[(df["TTM(days)"] >= start) & (df["TTM(days)"] <= stop)]
-
     xi, yi = np.meshgrid(x_axis, df["TTM(days)"])
-
     # Corresponding volatility values for each x_axis point
     zi = np.array([df["10DPut"].values,
                    df["25DPut"].values,
                    df["ATM"].values,
                    df["25DCall"].values,
                    df["10DCall"].values]).T
-
     fig = plt.figure(figsize=(10, 8))
-
     ax = fig.add_subplot(111, projection='3d')
-
     surf = ax.plot_surface(xi, yi, zi, cmap="viridis")
-
     ax.set_xlabel('Delta')
     ax.set_ylabel('Time to maturity (days)')
     ax.set_yticks(y_ticks)
     ax.set_yticklabels(y_legend)
-
     ax.set_zlabel('Volatility (in %)')
     ax.set_xticks(x_axis)
     ax.set_xticklabels(x_legend)
@@ -103,12 +93,53 @@ def plot_delta_vol(df, start=0, stop=30*365, save=False):
               fontweight="bold")
     ax.view_init(elev=25, azim=-45)  # Change these angles as per your need
     plt.legend()
-
     if save:
         plt.savefig("USDJPY Delta Volatility Surface.png", dpi=300)
-
     plt.show()
 
 
+def get_market_volatilty_surface(df, term_rate, base_rate, spot, start=0, stop=15*365, save=False):
+    """ 
+    input format:
+
+    TTM ATM 25DCall 25DPut  10DCall 25DPut TTM(days)
+    1D  11.1 10.1    14.52  11.81   9.65    1
+    1W  11.5 11.5    15.10  10.45   9.55    7
+                        ...
+    30Y  11.5 11.5   15.10  14.44  12.48   10950
+
+    output format df:
+
+    Strike  TTM  Vol    Forward
+    111.51  1   10.84   143.23
+    165.15  1   10.57   143.23    
+    ...
+    50.12   10950   12.65   53.18
+
+    """
+
+    # Only take wantyed part of DF
+    df = df.loc[(df["TTM(days)"] >= start) & (df["TTM(days)"] <= stop)]
+
+    delta = [0.5, 0.25, -0.25, 0.10, -0.10]
+    opt_sign = [1, 1, -1, 1, -1]
+
+    x, y, z = [], [], []  # Strike, TTM, Implied Vol -> To plot
+
+    df = df.drop(['TTM'], axis=1)
+
+    for index, row in df.iterrows():
+        TTM = row[-1]
+        FWD = pf.atm_forward(ttm=TTM, term_rate=term_rate,
+                             base_rate=base_rate, spot=spot)
+
+        for implied_vol in row[:-1]:
+            pf.get_strike()
+
+    return 0
+
+
+# Main()
 df = read_excel(file_path="bbgnoadj.xlsx")
-plot_delta_vol(df, start=31)
+get_market_volatilty_surface(
+    df, term_rate=JPY_RATE, base_rate=USD_RATE, spot=SPOT)
